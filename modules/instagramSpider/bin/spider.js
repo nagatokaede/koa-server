a76d85df0b79495b753bd5604d666091b795fd32
maozhiheng <zhiheng.mao@nx-engine.com>
2020-04-03 16:03:02 +0800

    fix: 调整爬虫

--- modules/instagramSpider/bin/spider.js
+++ modules/instagramSpider/bin/spider.js
@@ -8,13 +8,17 @@ const getHtml = url => {
 
     axios.get(url)
       .then(res => {
-        const reg = /(display_url":")[a-zA-z]+:\/\/[^"]*/ig;
-        // 初步解析内容
-        const content = body.match(reg);
-        // 进一步解析
-        const urlList = content.map(item => item.split('display_url":"')[1].replace(/\\u0026/g, '&'));
-        // 去重返回
-        resolve(new Set(urlList));
+        if (res.status === 200) {
+          const reg = /(display_url":")[a-zA-z]+:\/\/[^"]*/ig;
+          // 初步解析内容
+          const content = res.data.match(reg);
+          // 进一步解析
+          const urlList = content.map(item => item.split('display_url":"')[1].replace(/\\u0026/g, '&'));
+          // 去重返回
+          resolve(new Set(urlList));
+        } else {
+          reject(res);
+        }
       })
       .catch(err => {
         reject(err)
