4877d321819353ef286ba2b3f8c0b7b3a3963afa
nagatokaede <Nagato15498>
2020-04-16 22:36:15 +0800

    feat: 文件上传 middleware 化；对接 OSS 上传

---
+++ modules/upload/index.js
@@ -0,0 +1,97 @@
+'use static';
+
+const Busboy = require('busboy');
+const fs = require('fs');
+const path = require('path');
+const { mkdir } = require('../../util/fs');
+
+const upload = (ctx, options) => {
+  return new Promise((resolve, reject) => {
+    let field,url,msg;
+    // 创建目录
+    if (!options.streamFunction) mkdir(path.join(options.root, options.path));
+
+    const req = ctx.req;
+    const busboy = new Busboy({ headers: req.headers });
+  
+    busboy.on('file', function (fieldName, file, filename, encoding, mimeType) {
+      console.log(`File [${fieldName}]: filename: ${filename} encoding: ${encoding} mimeType: ${mimeType}`);
+      field = fieldName;
+      // 过滤
+      if(!options.filter.includes(path.extname(filename).slice(1).toLowerCase())) {
+        msg = 'upload file type error';
+        file.resume(); // 跳出上传
+        return 0;
+      }
+    
+      // 本地存储地址
+      const filePath = path.join(options.root, options.path, options.name || filename);
+
+      // 网络展示地址
+      url = path.join(options.path, options.name || filename).replace(/\\/g, '/');
+    
+      // 文件上传OSS或保存到特定路径
+      if (options.streamFunction) {
+        options.streamFunction(file, url);
+      } else {
+        file.pipe(fs.createWriteStream(filePath));
+      }
+    
+      // 开始解析文件流
+      file.on('data', function (data) {
+        console.info(`File [${fieldName}] got ${data.length} bytes`);
+      });
+    
+      // 解析文件结束
+      file.on('end', function () {
+        console.info(`File [${fieldName}] Finished`);
+      });
+    });
+
+    // 解析结束事件
+    busboy.on('finish', () => {
+      console.info('upload finish');
+      // 将地址传入上下文
+      if (!ctx.request.files) ctx.request.files = {};
+      ctx.request.files[field] = url || msg;
+      console.info(ctx.request.files);
+      resolve('upload finish');
+    });
+
+    // 解析错误事件
+    busboy.on('error', err => {
+      console.warn('upload error: ' + err);
+      reject('upload error');
+    });
+  
+    req.pipe(busboy);
+  });
+};
+
+/**
+ * 流上传文件可指定管道上传OSS
+ * @param {object} options - [ path(保存路径), name(保存文件名), streamFunction(接入流管道方法) ]
+ * @returns {Function}
+ */
+module.exports = (options = {}) => {
+  // 设置默认参数
+  Object.assign(options, {
+    root: options.root || path.join(__dirname, '../../public'),
+    path: options.path || 'upload',
+    filter: options.filter || [ 'jpg', 'jepg', 'png' ],
+  });
+
+  // 返回 middleware
+  return async (ctx, next) => {
+    if (ctx.headers['content-type'] && ctx.headers['content-type'].includes('multipart/form-data')) {
+      console.info('upload start');
+      try {
+        await upload(ctx, options);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    await next();
+  }
+};
+
--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ const fs = require('fs');
 const logger = require('../middleware/logger');
 const router = require('../middleware/router');
 const access = require('../middleware/access');
-const { mkdir } = require('../util/fs');
+
+const upload = require('../modules/upload');
+const { putStream } = require('../modules/oss');
 
 const app = new koa();
 
@@ -21,79 +23,12 @@ app.use(logger);
 
 app.use(access(['admin', 'upload'], '60m'));
 
-const upload = ctx => {
-  return new Promise((resolve, reject) => {
-    const req = ctx.req;
-    const busboy = new Busboy({ headers: req.headers });
-
-    const date = new Date().toLocaleString('zh').split(' ')[0].replace(/-/g, '');
-    const dirname = path.join(__dirname, '../public/upload/', date);
-
-    // 创建目录
-    mkdir(dirname);
-    console.info('upload', 'mkdir');
-
-    busboy.on('file', function(fieldName, file, filename, encoding, mimetype) {
-      console.log(`File [${fieldName}]: filename: ${filename}`);
-
-      const filePath = path.join(dirname, filename);
-      console.info(filePath);
-
-      // 将地址传入上下文
-      if (!ctx.request.files) ctx.request.files = {};
-      ctx.request.files[fieldName] = filePath;
-
-      console.info(ctx.request.files);
-
-      // 文件保存到特定路径
-      file.pipe(fs.createWriteStream(filePath));
-
-      // 开始解析文件流
-      file.on('data', function(data) {
-        console.log(`File [${fieldName}] got ${data.length} bytes`);
-      });
-
-      // 解析文件结束
-      file.on('end', function() {
-        console.log(`File [${fieldName}] Finished`);
-        console.info('upload');
-      });
-    });
-
-    // 解析表单中其他字段信息
-    busboy.on('field', function(fieldName, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('表单字段数据 [' + fieldName + ']: value: ' + inspect(val));
-      result.formData[fieldName] = inspect(val);
-    });
-
-    // 解析结束事件
-    busboy.on('finish', function( ) {
-      console.log('文件上结束');
-      resolve('upload finish')
-    });
-
-    // 解析错误事件
-    busboy.on('error', function(err) {
-      console.log('文件上出错');
-      reject('upload error')
-    });
-
-    req.pipe(busboy)
-  });
-};
-
-app.use(async (ctx, next) => {
-  if (ctx.headers['content-type'] && ctx.headers['content-type'].includes('multipart/form-data')) {
-    console.info('upload start');
-    try {
-      const msg = await upload(ctx);
-      console.info(msg);
-    } catch (e) {
-      console.warn(e);
-    }
-  }
-  await next();
-});
+// 上传文件到OSS
+app.use(upload({
+  streamFunction: (stream, name) => {
+    putStream(name, stream);
+  },
+}));
 
 app.use(bodyParser());
 
